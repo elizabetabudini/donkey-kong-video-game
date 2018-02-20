e33df4779d00807f36c307a02c4235a146fa319c
Luca <rispoli997@gmail.com>
2018-02-20 10:35:50 +0100

    created a new interface for entitites and directions

---
+++ src/model/characters/Direction.java
@@ -0,0 +1,25 @@
+package model.characters;
+
+
+/**
+ * Contains all the possible characters' directions.
+ * 
+ *
+ */
+public enum Direction {
+    /**
+     * The character is climbing.
+     */
+    UP,
+    
+    /*
+     * The character is moving right.
+     */
+    RIGHT,
+    
+    /**
+     * The character is moving left.
+     */
+    LEFT;
+
+}
---
+++ src/model/characters/Entity.java
@@ -0,0 +1,76 @@
+package model.characters;
+
+/**
+ * An interface modeling a basic Entity with methods to return and set the current position/direction.
+ *
+ */
+
+public interface Entity {
+
+    /**
+     * Returns the X coordinate of the entity.
+     * 
+     * @return The X coordinate
+     */
+    int getX();
+
+    /**
+     * Returns the Y coordinate of the entity.
+     * 
+     * @return The Y coordinate
+     */
+    int getY();
+
+    /**
+     * Sets the new coordinate for the X.
+     * 
+     * @param x
+     *            The new X.
+     */
+    void setX(int x);
+
+    /**
+     * Sets the new coordinate for the Y.
+     * 
+     * @param y
+     *            The new Y.
+     */
+    void setY(int y);
+
+    /**
+     * Moves the character in the dir direction.
+     * 
+     * @param dir
+     *            The direction chosen.
+     */
+    void move(Direction dir);
+
+    /**
+     * Returns the current direction.
+     * 
+     * @return The current direction.
+     */
+    Direction currentDirection();
+
+    /*
+     * Sets the new direction for the entity
+     * 
+     * @param dir The new direction.
+     */
+    void setDirection(Direction dir);
+
+    /**
+     * Returns whether or not the character is climbing stairs.
+     * 
+     * @return True if the character is climbing, false otherwise.
+     */
+    boolean isClimbing();
+
+    /**
+     * Returns whether or not the character is still alive.
+     * 
+     * @return True if the character is alive, false otherwise.
+     */
+    boolean isAlive();
+
+}

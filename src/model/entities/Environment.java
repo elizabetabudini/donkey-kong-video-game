958334a197b2597d15b566a559802aa7784f413b
marco <marco.creta@studio.unibo.it>
2018-03-12 19:21:56 +0100

    addition of the interface StaicEntity and other classes

---
+++ src/model/entities/Environment.java
@@ -0,0 +1,5 @@
+package model.entities;
+
+public interface Environment extends StaticEntity{
+
+}
--- src/model/entities/Princess.java
+++ src/model/entities/Princess.java
@@ -2,7 +2,7 @@ package model.entities;
 
 import java.awt.Dimension;
 
-public class Princess extends EntityImpl{
+public class Princess extends EntityImpl implements StaticEntity{
 
     public Princess(final Double x,final Double y, final Dimension dim) {
         super(x, y, dim);

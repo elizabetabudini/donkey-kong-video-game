dc5cac884bc3525263270631ef0162a5811729a5
Luca Rispoli <rispoli997@gmail.com>
2018-03-14 18:51:32 +0100

    started working on drawentities.java

--- src/view/entities/DrawEntitiesImpl.java
+++ src/view/entities/DrawEntitiesImpl.java
@@ -7,12 +7,19 @@ import model.entities.Entity;
 public class DrawEntitiesImpl implements DrawEntities{
     
     //constructor sarà usato per inizializzare la prima volta gli elementi statici (sfondo, pavimenti, scale)
-    public DrawEntitiesImpl() {
+    public DrawEntitiesImpl(final int height, final int width) {
         
     }
 
+    
+    
     @Override
     public void draw(final Map<Entity, String> entitiesToDraw) {
+        
+    }
+    
+    //return a bufferedImage
+    public void getImage() {
         
     }
 

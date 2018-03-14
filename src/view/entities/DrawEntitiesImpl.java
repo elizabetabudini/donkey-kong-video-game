f51999eb91e5c7670770bc4bfb092fecf9f1c338
Luca Rispoli <rispoli997@gmail.com>
2018-03-14 22:27:16 +0100

    add draw methods on drawEntities

--- src/view/entities/DrawEntitiesImpl.java
+++ src/view/entities/DrawEntitiesImpl.java
@@ -1,25 +1,39 @@
 package view.entities;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.util.Map;
 
 import model.entities.Entity;
 
 public class DrawEntitiesImpl implements DrawEntities{
     
+    private BufferedImage canvas;
+    final private int height;
+    final private int width;
+    
     //constructor sarà usato per inizializzare la prima volta gli elementi statici (sfondo, pavimenti, scale)
     public DrawEntitiesImpl(final int height, final int width) {
-        
+        canvas = new BufferedImage(width, height,BufferedImage.TYPE_INT_ARGB);
+        this.height = height;
+        this.width = width;
     }
 
     
     //metodo da chiamare per tutte le entità da disegnare sulla bufferedimage
     @Override
     public void draw(final Map<Entity, String> entitiesToDraw) {
-        
+        Graphics g = canvas.getGraphics();
+        g.drawimage();
     }
     
     //return a bufferedImage, da usare in paintcomponent()
-    public void getImage() {
+    public BufferedImage getImage() {
+        scaleImage();
+        return canvas;
+    }
+    
+    private void scaleImage() {
         
     }
 

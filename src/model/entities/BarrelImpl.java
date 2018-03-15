b78c6fe84d63ab5dba949c2ef311eb1ac6c23700
Samuele Gregori <samuele.gregori@studio.unibo.it>
2018-03-15 23:25:09 +0100

    Earlu
    implementation of tryToMove

--- src/model/entities/BarrelImpl.java
+++ src/model/entities/BarrelImpl.java
@@ -1,17 +1,14 @@
 package model.entities;
 
 import java.awt.Dimension;
+import java.util.Optional;
 
 public class BarrelImpl extends DynamicEntityImpl implements Barrel, DynamicEntity {
 
+    private final static double STEP = 1;
+    
     public BarrelImpl(final Double x, final Double y, final Dimension dim) {
         super(x,y,dim);
-    }
-
-    @Override
-    protected void tryToMove(Movement dir) {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
@@ -23,6 +20,21 @@ public class BarrelImpl extends DynamicEntityImpl implements Barrel, DynamicEnti
     @Override
     public boolean isColliding(final Entity entity) {
         return this.getHitbox().intersects(entity.getHitbox());
+    }
+
+    @Override
+    protected void tryToMove(Optional<Movement> dir) {
+        /*Factorize this part with MarioImpl*/
+        if(!dir.isPresent()) {
+            return;
+        }
+        this.setDirection(dir.get());
+        if (dir.get() == Movement.LEFT) {
+            this.setDeltaX(-STEP);
+        } else if (dir.get() == Movement.RIGHT) {
+            this.setDeltaX(STEP);
+        }
+        /*TODO*/
     }
 
 
--- src/model/test/BarrelTest.java
+++ src/model/test/BarrelTest.java
@@ -1,5 +1,7 @@
 package model.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.awt.Dimension;
@@ -22,6 +24,15 @@ public class BarrelTest {
         final Barrel simpleBarrel = bf.createSimpleBarrel(10.0, 20.0, new Dimension(100,100));
         final Mario tester = new MarioImpl(9.0, 20.0, new Dimension(100,100));
         tester.move(Optional.of(Movement.RIGHT));
+        assertTrue("The barrel was supposed to hit Mario", simpleBarrel.isColliding(tester));
+        /*TODO tester.move(Optional.of(Movement.LEFT));
+        assertFalse("The barrel was not supposed to hit Mario", simpleBarrel.isColliding(tester));*/
+        for(int i=0; i < 100; i++) {
+            simpleBarrel.move(Optional.of(Movement.RIGHT));
+            tester.move(Optional.of(Movement.RIGHT));
+            /*TODO CORRECT the return value description in Entity class*/
+        }
+        assertEquals("The barrel was supposed to be in the right position",Double.valueOf(110.0), Double.valueOf(simpleBarrel.getX()));
         assertTrue("The barrel was supposed to hit Mario", simpleBarrel.isColliding(tester));
     }
 

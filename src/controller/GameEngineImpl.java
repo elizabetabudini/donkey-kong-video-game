16637652bfc5557b609af8a9650843ed7de206df
Samuele Gregori <samuele.gregori@studio.unibo.it>
2018-04-06 21:43:20 +0200

    implementation of processInput

--- src/controller/GameEngineImpl.java
+++ src/controller/GameEngineImpl.java
@@ -1,33 +1,44 @@
 package controller;
 
 import java.util.List;
-
-import javax.swing.JPanel;
-
+import java.util.Optional;
+import java.util.Set;
 import model.entities.Barrel;
-import model.entities.BarrelFactory;
-import model.entities.Entity;
-import model.test.GameLoopTest;
+import model.entities.Mario;
+import model.entities.Movement;
+import view.InputHandler;
 
 public class GameEngineImpl implements GameEngine {
     
-    private final long period = 20;
-    private final Entity mario;
+    private final static long PERIOD = 20;
+    private final Mario mario;
     private final List<Barrel> barrels;
     private final GameLoop gameLoop;
+    private final InputTranslator translator;
+    private final InputHandler handler  = new InputHandler();
  
-    public GameEngineImpl(Entity mario, List<Barrel> barrels) {
+    public GameEngineImpl(final Mario mario, final List<Barrel> barrels) {
         super();
        // this.board = new Board(barrels.get(0));
         this.mario = mario;
         this.barrels = barrels;
         this.gameLoop = new GameLoop();
-        
+        translator = input->{
+            switch (input) {
+            case ARROW_DOWN: return Movement.DOWN;
+            case ARROW_LEFT: return Movement.LEFT;
+            case ARROW_RIGHT: return Movement.RIGHT;
+            case ARROW_UP: return Movement.UP;
+            case SPACE: return Movement.JUMP;
+            default: return null;
+            }
+        };
+        /* ***in the view addKeyListener(handler); */     
     }
 
     @Override
     public void startGame() {
-        this.gameLoop.start();
+        this.gameLoop.start();      
     }
 
     /**
@@ -38,32 +49,47 @@ public class GameEngineImpl implements GameEngine {
      */
     private void waitNextFrame(final long currentTime) {
        final long delta = System.currentTimeMillis() - currentTime;
-       if (delta < this.period) {
+       if (delta < PERIOD) {
            try {
-               Thread.sleep(period - delta);
+               Thread.sleep(PERIOD - delta);
            } catch (Exception ex){}
        }
     }
 
     private void render() {
         // TODO Auto-generated method stub
-        
     }
 
     private void updateGame(long elapsedTime) {
       // this.mario.update(elapsedTime); //??
-       //this.Barrels.forEach(br -> br.update(elapsedTime)); 
+       //this.barrels.forEach(br -> br.update(elapsedTime)); 
     }
 
+    /**
+     * Translates each key-input using methods 
+     * of functional interface {@link InputTranslator}
+     */
     private void processInput() {
-        // TODO Auto-generated method stub
+        Set<Movement> parsedMovements = translator.inputParser(handler.parser(false));
+        
+        for (final Movement dir : parsedMovements) {
+            mario.stopMoving(dir);
+        }
+
+        parsedMovements = translator.inputParser(handler.parser(true));
+
+        for (final Movement dir : parsedMovements) {
+            mario.move(Optional.of(dir));
+        }
+       // mario.update(); /* ?*/
         
     }
     
     private class GameLoop extends Thread {
             
-            public GameLoop() {
-                    }
+            private GameLoop() {
+                    super();
+                   }
             
             public void run() {
                 final long lastLoopTime = System.currentTimeMillis();

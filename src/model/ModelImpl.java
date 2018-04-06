9abb946fa6b1ff39b11eb1cf3f80ba6b23e95bdf
marco <marco.creta@studio.unibo.it>
2018-04-06 15:19:47 +0200

    some model classes edits

--- src/model/ModelImpl.java
+++ src/model/ModelImpl.java
@@ -1,11 +1,35 @@
 package model;
 
+import model.entities.DonkeyKong;
+import model.entities.Mario;
+import model.entities.Princess;
+import model.levels.BasicLevel;
 
+import java.util.List;
+
+import model.entities.Barrel;
 
 public class ModelImpl implements ModelInterface{
-    public final static int HEIGHT = 540;
-    public final static int WIDTH = 460;
-    public final static double GRAVITY = 0.030;
     
 
+    public final static int HEIGHT = 540;
+    public final static int WIDTH = 460;
+    
+    public final static double GRAVITY = 0.030;
+
+    public final static int PLAYER_LIFE = 3;
+    
+    //game info
+    private GameStatus gameStatus;
+    private BasicLevel currentLevel;
+    
+    //player info
+    private int score;
+    private int PlayerLivesLeft;
+    
+    //entities info
+    private Mario mario;
+    private DonkeyKong donkeyKong;
+    private Princess princess;
+    private List<Barrel> barrel;
 }
--- src/model/ModelInterface.java
+++ src/model/ModelInterface.java
@@ -1,11 +1,72 @@
 package model;
 
+import java.util.List;
+
+import model.entities.Barrel;
+import model.entities.Mario;
+import model.levels.BasicLevel;
+
 /**
  * The interface that defines the main methods to build the game.
  */
 
 public interface ModelInterface {
     
+    /**
+     * Getter for the current Level in game.
+     * 
+     * @return the current level.
+     */
+    BasicLevel getCurrentLevel();
+
+    /**
+     * Getter for the Score.
+     * 
+     * @return the player score.
+     */
+    int getScore();
+
+    /**
+     * Getter for the remaining lives.
+     * 
+     * @return the number of the lives the player can lose before game over.
+     */
+    int getLife();
+
+    /**
+     * Getter for Mario.
+     * 
+     * @return the main entity controlled by the player.
+     */
+    Mario getPaddle();
     
+    /**
+     * Getter for DonkeyKong.
+     * 
+     * @return the entity controlled by the game.
+     */
+    Mario getDonkeyKong();
+    
+    /**
+     * Getter for Princess.
+     * 
+     * @return the entity that sign the victory.
+     */
+    Mario getPrincess();
+
+    /**
+     * Getter for the Barrels.
+     * 
+     * @return the list of all the active barrels.
+     */
+    List<Barrel> getBalls();
+
+    /**
+     * Getter for the Game Status.
+     * 
+     * @return a game status defined in the enum {@link GameStatus}.
+     */
+    GameStatus getGameStatus();
+
 
 }

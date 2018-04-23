1725adf32cd2ecc2d172b80c3bd894d981fd6e2e
elizabeta.budini@studio.unibo.it <elizabeta.budini@studio.unibo.it>
2018-04-23 21:28:26 +0200

    name labels

--- src/view/menuPanels/InfoPanel.java
+++ src/view/menuPanels/InfoPanel.java
@@ -7,6 +7,8 @@ import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import javafx.scene.layout.Border;
 import utilities.ImageLoader;
 import view.BackgroundPanel;
 
@@ -21,33 +23,39 @@ public class InfoPanel extends JPanel {
     private static final long serialVersionUID = 1L;
 
     public InfoPanel() {
-        JLabel labelcred = new JLabel("Credits:");
-        labelcred.setFont(new Font("Arial", Font.BOLD, 30));
-        JLabel lab1 = new JLabel("Luca Rispoli");
-        JLabel lab2 = new JLabel("Samuele Gregori");
-        JLabel lab3 = new JLabel("Marco Creta");
-        JLabel lab4 = new JLabel("Elizabeta Budini");
+        
+        ImageIcon background = ImageLoader.getInstance().getImage("images/background2.jpg");
+        ImageIcon credits = ImageLoader.getInstance().getImage("images/background2.jpg");
+        ImageIcon elizabeta = ImageLoader.getInstance().getImage("images/background2.jpg");
+        ImageIcon luca = ImageLoader.getInstance().getImage("images/background2.jpg");
+        ImageIcon marco = ImageLoader.getInstance().getImage("images/background2.jpg");
+        ImageIcon samuele = ImageLoader.getInstance().getImage("images/background2.jpg");
+        
+        JLabel labelcred = new JLabel();
+        labelcred.setIcon(credits);
+        JLabel lab1 = new JLabel();
+        lab1.setIcon(luca);
+        JLabel lab2 = new JLabel();
+        lab2.setIcon(samuele);
+        JLabel lab3 = new JLabel();
+        lab3.setIcon(marco);
+        JLabel lab4 = new JLabel();
+        lab4.setIcon(elizabeta);
+        
         JPanel panelName = new JPanel();
         panelName.setLayout(new BoxLayout(panelName, BoxLayout.Y_AXIS));
-        panelName.add(labelcred);
         panelName.add(lab1);
         panelName.add(lab2);
         panelName.add(lab3);
         panelName.add(lab4);
 
-        ImageIcon background = ImageLoader.getInstance().getImage("images/background2.jpg");
-
         BackgroundPanel backgroundPanel = new BackgroundPanel(background.getImage(), BackgroundPanel.SCALED, 0.0f,
                 0.0f);
 
         backgroundPanel.setLayout(new BorderLayout());
-        JPanel buttonsPanel = new JPanel(new FlowLayout());
-        buttonsPanel.add(labelcred);
-        buttonsPanel.add(lab1);
-        buttonsPanel.add(lab2);
-        buttonsPanel.add(lab3);
-        buttonsPanel.add(lab4);
-        backgroundPanel.add(buttonsPanel, BorderLayout.SOUTH);
+   
+        backgroundPanel.add(panelName, BorderLayout.CENTER);
+        backgroundPanel.add(labelcred, BorderLayout.NORTH);
 
         this.setLayout(new BorderLayout());
         this.add(backgroundPanel);

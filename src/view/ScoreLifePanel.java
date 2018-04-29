37e15371ebb58ba1c7c4315251626a80e178a525
Luca <luca.rispoli@studio.unibo.it>
2018-04-29 11:43:21 +0200

    Updated score on gamescreen

--- src/view/ScoreLifePanel.java
+++ src/view/ScoreLifePanel.java
@@ -35,7 +35,7 @@ public class ScoreLifePanel extends JPanel {
         setBackground(Color.RED);
         score.setHorizontalAlignment(SwingConstants.CENTER);
         this.add(score);
-        updateScore(-1,-1);
+        updateScore(-1,-1,-1.0);
     }
 
     /**
@@ -44,13 +44,13 @@ public class ScoreLifePanel extends JPanel {
      * @param score
      *            the current score of the player
      */
-    public static void updateScore(final int lives, final int score) {
+    public static void updateScore(final int lives, final int score, final Double diff) {
         SwingUtilities.invokeLater(() -> {
             if(lives == 0) {
                 ScoreLifePanel.score.setText("GAME OVER");
             }
             else {
-                ScoreLifePanel.score.setText("Lives: " + lives + " | Score: " + score);
+                ScoreLifePanel.score.setText("Lives: " + lives + " | Score: " + score + "| Diff:"+diff.intValue());
             }
 
         });
